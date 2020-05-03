5a29fbed7d056a6576b6c0a9f11134bdb4bb4163
Lucie Strakova <strakova.91@gmail.com>
2020-05-03 13:17:08 +0200

    forgot password

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -42,16 +42,16 @@
       <b-button type="submit" variant="primary" @click="login">Přihlásit se</b-button>
 
       <div class="extras">
-        <a>Zapomenuté heslo</a>
+        <a @click="togglePasswordReset">Zapomenuté heslo</a>
         <span>|</span>
         <a @click="toggleForm">Vytvořit účet</a>
       </div>
 
     </b-form>
 
-    <b-form v-if="!showLoginForm" @submit.prevent>
+    <b-form v-if="!showLoginForm && !showForgotPassword" @submit.prevent>
 
-      <h1 class="signup-headline">Nový účet</h1>
+      <h1 class="smaller-headline">Nový účet</h1>
 
       <b-form-group
         id="name"
@@ -117,6 +117,52 @@
 
     </b-form>
 
+    <b-form v-if="showForgotPassword" @submit.prevent class="password-reset">
+
+      <div v-if="!passwordResetSuccess">
+
+        <h1 class="smaller-headline">Obnovení hesla</h1>
+        <p>Zašleme Vám email s odkazem pro změnu hesla</p>
+
+        <b-form-group
+          id="email3"
+          label="Email:"
+          label-for="email3"
+          description=""
+        >
+          <b-form-input
+            id="email3"
+            v-model.trim="passwordForm.email"
+            type="text"
+            placeholder=""
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="warning" @click="resetPassword">Změnit heslo</b-button>
+
+        <div class="extras">
+          <a @click="togglePasswordReset">Zpět na přihlášení</a>
+        </div>
+
+      </div>
+
+      <div v-else>
+
+        <h1 class="smaller-headline">Email byl odeslán</h1>
+        <p>Zkontrolujte si prosím svou emailovou schránku</p>
+
+        <b-button type="submit" variant="info" @click="togglePasswordReset">Zpět na přihlášení</b-button>
+
+      </div>
+
+    </b-form>
+
+    <transition name="fade">
+      <div v-if="errorMsg !== ''" class="error-msg">
+        <p>{{ errorMsg }}</p>
+      </div>
+    </transition>
+
   </div>
 </template>
 
@@ -137,13 +183,40 @@ export default {
         email: '',
         password: ''
       },
+      passwordForm: {
+        email: ''
+      },
       showLoginForm: true,
-      performingRequest: false
+      performingRequest: false,
+      passwordResetSuccess: false,
+      showForgotPassword: false,
+      errorMsg: ''
     }
   },
   methods: {
     toggleForm () {
+      this.loginForm = {
+        email: '',
+        password: ''
+      }
+      this.signupForm = {
+        name: '',
+        surname: '',
+        email: '',
+        password: ''
+      }
+      this.errorMsg = ''
       this.showLoginForm = !this.showLoginForm
+    },
+    togglePasswordReset () {
+      if (this.showForgotPassword) {
+        this.showLoginForm = true
+        this.showForgotPassword = false
+        this.passwordResetSuccess = false
+      } else {
+        this.showLoginForm = false
+        this.showForgotPassword = true
+      }
     },
     login () {
       this.performingRequest = true
@@ -157,7 +230,7 @@ export default {
       }).catch(err => {
         // on reject
         this.performingRequest = false
-        console.log(err)
+        this.errorMsg = err.message
       })
     },
     signup () {
@@ -173,11 +246,25 @@ export default {
           this.performingRequest = false
           this.$router.push('/')
         }).catch(err => {
-          console.log(err)
+          this.performingRequest = false
+          this.errorMsg = err.message
         })
       }).catch(err => {
-        this.performingRequest = true
+        this.performingRequest = false
+        this.errorMsg = err.message
+      })
+    },
+    resetPassword () {
+      this.performingRequest = true
+
+      fb.auth.sendPasswordResetEmail(this.passwordForm.email).then(() => {
+        this.performingRequest = false
+        this.passwordResetSuccess = true
+        this.passwordForm.email = ''
+      }).catch(err => {
         console.log(err)
+        this.performingRequest = false
+        this.errorMsg = err.message
       })
     }
   }
